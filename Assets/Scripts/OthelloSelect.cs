76a550ef3b178393bf2fc11c0a024a85ba3f5d08
Lacty <akira206@gmail.com>
2015-11-05 18:15:00 +0900

    select追加

---
+++ Assets/Scripts/OthelloSelect.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OthelloSelect : MonoBehaviour {
+  
+  private int WIDTH;
+  private int DEPTH;
+  
+  [SerializeField]
+  Material _selected = null;
+  public Material SelectedMaterial
+  {
+    get {
+      if (_selected != null) { return _selected; }
+      _selected = Resources.Load<Material>("Cell/Selected");
+      return _selected;
+    }
+  }
+
+  [SerializeField]
+  Material _normal = null;
+  public Material NormalMaterial
+  {
+    get {
+      if (_normal != null) { return _normal; }
+      _normal = Resources.Load<Material>("Cell/Normal");
+      return _normal;
+    }
+  }
+  
+  private void ChangeColor() {
+    var cell = GameObject.Find("OthelloTable/Cell(" + _selectX + "," + _selectZ + ")");
+    cell.GetComponent<Renderer>().material = SelectedMaterial;
+  }
+  
+  private void backColor() {
+    var cell = GameObject.Find("OthelloTable/Cell(" + _selectX + "," + _selectZ + ")");
+    cell.GetComponent<Renderer>().material = NormalMaterial;
+  }
+  
+  private int _selectX = 0;
+  private int _selectZ = 0;
+
+	void Start () {
+		var table = GameObject.Find("OthelloTable");
+    WIDTH = table.GetComponent<OthelloTableView>().GetWidth();
+    DEPTH = table.GetComponent<OthelloTableView>().GetDepth();
+    
+    ChangeColor();
+	}
+
+	void Update () {
+    bool keyW = Input.GetKeyDown(KeyCode.W);
+    bool keyA = Input.GetKeyDown(KeyCode.A);
+    bool keyS = Input.GetKeyDown(KeyCode.S);
+    bool keyD = Input.GetKeyDown(KeyCode.D);
+    
+    if (keyW && (_selectX < DEPTH - 1) && (_selectX >= 0)) {
+      backColor();
+      _selectX++;
+      ChangeColor();
+    }
+    if (keyA && (_selectZ <= WIDTH - 1) && (_selectZ > 0)) {
+      backColor();
+      _selectZ--;
+      ChangeColor();
+    }
+    if (keyS && (_selectX <= DEPTH - 1) && (_selectX > 0)) {
+      backColor();
+      _selectX--;
+      ChangeColor();
+    }
+    if (keyD && (_selectZ < WIDTH - 1) && (_selectZ >= 0)) {
+      backColor();
+      _selectZ++;
+      ChangeColor();
+    }
+	}
+}
--- Assets/Scripts/OthelloTableView.cs
+++ Assets/Scripts/OthelloTableView.cs
@@ -3,6 +3,9 @@ using System.Collections;
 
 public class OthelloTableView : MonoBehaviour {
   
+  private int WIDTH = 8;
+  private int DEPTH = 8;
+  
   [SerializeField]
   private GameObject _cell = null;
   public GameObject Cell{
@@ -14,13 +17,11 @@ public class OthelloTableView : MonoBehaviour {
     }
   }
   
-	void Start () {
-    int width = 8;
-    int depth = 8;
+  void Start () {
     float offset = 1.01f;
     var table = GameObject.Find("OthelloTable");
-    for (int d = -depth / 2; d < depth; ++d) {
-      for (int w = -width / 2; w < width; ++w) {
+    for (int d = 0; d < DEPTH; ++d) {
+      for (int w = 0; w < WIDTH; ++w) {
 	      var cell = Instantiate(Cell);
         cell.name = Cell.name + "(" + d + "," + w + ")";
         cell.transform.parent = table.transform;
@@ -29,5 +30,13 @@ public class OthelloTableView : MonoBehaviour {
                                                    d * offset);
       }
     }
-	}
+  }
+  
+  public int GetWidth() {
+    return WIDTH;
+  }
+  
+  public int GetDepth() {
+    return DEPTH;
+  }
 }
